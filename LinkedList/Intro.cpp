a42005408051d568769f3638091e20a1da71bc12
divyanshuu8 <singhdivyanshu975@gmail.com>
2025-06-23 11:20:31 +0530

    first commit

---
+++ LinkedList/Intro.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Node {
+    int data;
+    Node* next;
+
+    public:
+    Node(int val){
+        data = val;
+        next = nullptr;
+    }
+    void AddNode(int val, Node* head){
+        Node* temp = new Node(val);
+        temp->next = nullptr;
+        head->next = temp;
+    } 
+
+    void print(Node* head){
+        while(head != nullptr){
+            cout<< head->data << " ";
+            head = head->next;
+        }
+    }
+    
+};
+Node* ConvertArrLL(int arr[]){
+    Node* temp = new Node(arr[0]);
+    Node* Mover = temp;
+    for(int i = 0; i<4; i++){
+        Mover->AddNode(arr[i], temp);
+    }
+    return temp;
+}
+
+
+int main() {
+    int arr[4] = {1, 2, 3, 4};
+
+    Node* LL = ConvertArrLL(arr);
+    LL->print(LL);
+
+
+
+
+    return 0;
+}
